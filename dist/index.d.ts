b8d915967a6d118ef4840d212e9bcb2ba759231e
kang.chen <kang.chen@coupang.com>
2019-06-02 15:12:38 +0800

    add method decorator

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export * from './core/Bootstrap';
 export * from './core/ClassInject';
+export * from './core/MethodInject';
 export * from './core/PropInject';
--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 tslib_1.__exportStar(require("./core/Bootstrap"), exports);
 tslib_1.__exportStar(require("./core/ClassInject"), exports);
+tslib_1.__exportStar(require("./core/MethodInject"), exports);
 tslib_1.__exportStar(require("./core/PropInject"), exports);
 //# sourceMappingURL=index.js.map
